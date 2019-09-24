6237af0aebd0a79e73f294dde5cb9fc9cf96c494
wenqi73 <1264578441@qq.com>
2019-09-24 21:10:18 +0800

    release: 8.3.1

--- prerender/sitemap.js
+++ prerender/sitemap.js
@@ -5,20 +5,23 @@ var path_1 = require("path");
 var sitemap = require("sitemap");
 var static_paths_1 = require("./static.paths");
 var build_config_1 = require("../build-config");
-var urls = Array.from(new Set(static_paths_1.ROUTES.filter(function (r) { return r !== '/'; }).map(function (r) { return r.replace(/\/(zh|en)$/, ''); }))).map(function (r) {
-    return {
-        url: r + "/en",
-        changefreq: 'weekly',
-        priority: r.includes('docs') ? 0.5 : 0.8,
-        links: [{ lang: 'en', url: r + "/en" }, { lang: 'zh', url: r + "/zh" }]
-    };
+function generateUrls(lang) {
+    var urls = Array.from(new Set(static_paths_1.ROUTES.filter(function (r) { return r !== '/'; }).map(function (r) { return r.replace(/\/(zh|en)$/, ''); })));
+    return urls.map(function (r) {
+        return {
+            url: r + "/" + lang,
+            changefreq: sitemap.EnumChangefreq.WEEKLY,
+            priority: r.includes('docs') ? 0.5 : 0.8,
+            links: [{ lang: 'en', url: r + "/en" }, { lang: 'zh', url: r + "/zh" }]
+        };
+    });
 }
-// tslint:disable-next-line:no-any
-);
 var sitemapInstance = sitemap.createSitemap({
     hostname: 'https://ng.ant.design',
     cacheTime: 600000,
-    urls: [{ url: '/', changefreq: 'weekly', priority: 0.5, lastmodrealtime: true }].concat(urls)
+    urls: [
+        { url: '/', changefreq: sitemap.EnumChangefreq.WEEKLY, priority: 0.5, lastmodrealtime: true }
+    ].concat(generateUrls('en'), generateUrls('zh'))
 });
-fs_1.writeFileSync(path_1.join(build_config_1.buildConfig.outputDir, 'sitemap.xml'), sitemapInstance.toString());
+fs_1.writeFileSync(path_1.join(build_config_1.buildConfig.outputDir, 'sitemap.xml'), sitemapInstance.toString(true));
 //# sourceMappingURL=sitemap.js.map
